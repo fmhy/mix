f0a66303ec88235354f1f8844f577369d9956bd1
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-02-14 18:11:29 +0000

    feat(parser): extractSections, extractItems

--- packages/parser/src/visit.ts
+++ packages/parser/src/visit.ts
@@ -1,4 +1,4 @@
-import { visit } from 'unist-util-visit'
+import { select } from 'unist-util-select'
 import type { Node } from 'unist'
 
 export interface Section {
@@ -9,19 +9,36 @@ export interface Section {
   }[]
 }
 
-export function parseSections() {
+export function extractSections(ast: any): Section[] {
   const sections: Section[] = []
-  let currentHeading = ''
-  let currentList = []
 
-  const transformer = (tree: Node) => {
-    visit(tree, 'heading', (node) => {
-      if (currentHeading && currentList.length > 0) {
-        sections.push({ heading: currentHeading, items: currentList })
-        currentList = []
+  let currentSection: Section | null = null
+
+  ast.children.forEach((node: Node) => {
+    if (node.type === 'heading') {
+      currentSection = {
+        heading: node.children![0].value as string,
+        items: []
       }
-      currentHeading = node.children[0].value
-    })
-  }
-  return transformer
+      sections.push(currentSection)
+    } else if (currentSection && node.type === 'list') {
+      extractItems(node, currentSection.items)
+    }
+  })
+
+  return sections
+}
+
+export function extractItems(
+  node: any,
+  items: { title: string; url: string }[]
+): void {
+  node.children.forEach((li: any) => {
+    const linkNode = select('link', li)
+    if (linkNode) {
+      const title = linkNode.children[0].value as string
+      const url = linkNode.url as string
+      items.push({ title, url })
+    }
+  })
 }
---
+++ playground/index.md
@@ -0,0 +1,9 @@
+## Section 1
+
+- [link 1](link1)
+- [link 2](link2)
+
+## Section 2
+
+- [link 1](link1)
+- [link 2](link2)
